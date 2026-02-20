7df8324c940752f8357ad3d4c7a09ea550c266c2
zhenni-17 <ST10468354@vcconnect.edu.za>
2026-02-20 15:17:45 +0200

    Print statement

---
+++ JavaTest_Github/src/javatest_github/JavaTest_Github.java
@@ -0,0 +1,40 @@
+
+package javatest_github;
+//import java.util.Scanner;
+
+/**
+ *
+ * @author zhennilu
+ */
+// access modifiers - private, public, protected
+// file is Github_Java
+// github_java is the class therefor class is file
+public class JavaTest_Github {
+/*
+    { this is a block}
+    */
+    /**
+     * @param args the command line arguments
+     */
+    // this statement is a method 
+    // static is one copy of method, there is one instance
+    //of the method
+    //void - returns nothing
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // data types
+        // variables
+        // [R][i][K][o]
+        String myName = "Zhenni";
+        char intial = 'h';
+        int myAge = 8;
+        double myExactAge = 8.95;
+        
+        System.out.println("Hello" + " " + myName + "!");
+        System.out.println("Please enter your ID:");
+    }
+    // int is a data type
+    int methodExample(){
+        
+    }
+}
